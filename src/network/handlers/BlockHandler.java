3b3afd8ff15ff7c15f187b4e44183ad85510b0ce
Mahmoud Gamal <mahmoud.gamal.15497@gmail.com>
2020-05-07 21:21:03 +0200

    added remaining data structures

---
+++ src/network/handlers/BlockHandler.java
@@ -0,0 +1,15 @@
+package network.handlers;
+
+import network.entities.CommunicationUnit;
+
+public class BlockHandler implements Handler {
+    @Override
+    public void handleOutgoing(CommunicationUnit cu) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void handleIncoming(CommunicationUnit cu) {
+        throw new UnsupportedOperationException();
+    }
+}
