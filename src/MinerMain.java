7ee2f4e8b003ce7bbd64eb49d848a7d67c6205ca
mostafaLabib65 <mostafa.labib65@gmail.com>
2020-05-14 03:07:53 +0200

    Testing Mains

---
+++ src/MinerMain.java
@@ -0,0 +1,12 @@
+import Nodes.Consensus.Consensus;
+import Nodes.Consensus.POWBlockConsumer;
+import Nodes.Miner.Miner;
+import Nodes.Miner.POWMiner;
+
+public class MinerMain {
+
+    public static void main(String[] args){
+        Consensus consensus = new POWBlockConsumer(2);
+        Miner miner = new POWMiner(consensus, 1, "127.0.0.1", 5000, true, 2);
+    }
+}
